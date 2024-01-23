475dcc52aaf3880378104e07b54ee1f45f38c293
Andy-Zi <andreas@zieglerit.org>
2024-01-23 19:54:49 +0100

    undotree update

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -4,6 +4,9 @@ return {
           vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
           vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
           vim.opt.undofile = true
+          vim.opt.undolevels = 10000
+          vim.g.undotree_WindowLayout = 2
+
           local wk = require("which-key")
           wk.register({
                ["<leader>-u"] = { vim.cmd.UndoTreeToggle, "Undo Tree" },
