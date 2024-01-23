b6a2f6740eb6d45107197f5c90f69b2c3343a135
Andy-Zi <andreas@zieglerit.org>
2024-01-23 19:23:52 +0100

    tmux navigator update

--- lua/plugins/vim-tmux-navigator.lua
+++ lua/plugins/vim-tmux-navigator.lua
@@ -1,17 +1,27 @@
 return {
-  "christoomey/vim-tmux-navigator",
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-    "TmuxNavigatePrevious",
-  },
-  keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-  },
+     "christoomey/vim-tmux-navigator",
+     cmd = {
+          "TmuxNavigateLeft",
+          "TmuxNavigateDown",
+          "TmuxNavigateUp",
+          "TmuxNavigateRight",
+          "TmuxNavigatePrevious",
+     },
+     keys = {
+          { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+          { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+          { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+          { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+          { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+     },
+     setup = function() 
+          local wk = require("which-key")
+          wk.register({
+               ["<c-h>"] = { "<cmd><C-U>TmuxNavigateLeft<cr>", "TmuxNavigateLeft" },
+               ["<c-j>"] = { "<cmd><C-U>TmuxNavigateDown<cr>", "TmuxNavigateDown" },
+               ["<c-k>"] = { "<cmd><C-U>TmuxNavigateUp<cr>", "TmuxNavigateUp" },
+               ["<c-l>"] = { "<cmd><C-U>TmuxNavigateRight<cr>", "TmuxNavigateRight" },
+               ["<c-\\>"] = { "<cmd><C-U>TmuxNavigatePrevious<cr>", "TmuxNavigatePrevious" },
+          }, {})
+     end,
 }
