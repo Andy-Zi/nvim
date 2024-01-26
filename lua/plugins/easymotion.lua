cc326d83ecce7fff4ecd0cc2f3ec0543637436a7
Andy-Zi <andreas@zieglerit.org>
2024-01-26 18:29:38 +0100

    easy motion keys

--- lua/plugins/easymotion.lua
+++ lua/plugins/easymotion.lua
@@ -2,7 +2,7 @@ return {
 	"easymotion/vim-easymotion",
 	config = function()
 		-- Disable default mappings
-		vim.g.EasyMotion_do_mapping = 0
+		vim.g.EasyMotion_do_mapping = 1
 
 		-- Custom mappings
 		vim.api.nvim_set_keymap("n", "s", "<Plug>(easymotion-overwin-f2)", { noremap = false })
@@ -13,5 +13,13 @@ return {
 		-- JK motions: Line motions
 		vim.api.nvim_set_keymap("n", "<Leader>j", "<Plug>(easymotion-j)", { noremap = false })
 		vim.api.nvim_set_keymap("n", "<Leader>k", "<Plug>(easymotion-k)", { noremap = false })
+          local wk = require("which-key")
+          wk.register({
+            ["<Leader>j"] = { "<Plug>(easymotion-j)", "Find line below" },
+            ["<Leader>k"] = { "<Plug>(easymotion-k)", "Find line above" },
+          })
+          wk.reregister({
+               ["s"] = { "<Plug>(easymotion-overwin-f2)", "Find 2 characters" },
+          })
 	end,
 }
