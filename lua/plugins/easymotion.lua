e105f587d919e25492308085366743671871df12
Andy-Zi <andreas@zieglerit.org>
2024-01-27 15:08:12 +0100

    updated which key to include most everything

--- lua/plugins/easymotion.lua
+++ lua/plugins/easymotion.lua
@@ -1,25 +1,25 @@
 return {
-	"easymotion/vim-easymotion",
-	config = function()
-		-- Disable default mappings
-		vim.g.EasyMotion_do_mapping = 1
+     "easymotion/vim-easymotion",
+     config = function()
+          -- Disable default mappings
+          vim.g.EasyMotion_do_mapping = 1
 
-		-- Custom mappings
-		vim.api.nvim_set_keymap("n", "s", "<Plug>(easymotion-overwin-f2)", { noremap = false })
+          -- Custom mappings
+          vim.api.nvim_set_keymap("n", "s", "<Plug>(easymotion-overwin-f2)", { noremap = false })
 
-		-- Turn on case-insensitive feature
-		vim.g.EasyMotion_smartcase = 1
+          -- Turn on case-insensitive feature
+          vim.g.EasyMotion_smartcase = 1
 
-		-- JK motions: Line motions
-		vim.api.nvim_set_keymap("n", "<Leader>j", "<Plug>(easymotion-j)", { noremap = false })
-		vim.api.nvim_set_keymap("n", "<Leader>k", "<Plug>(easymotion-k)", { noremap = false })
+          -- JK motions: Line motions
+          vim.api.nvim_set_keymap("n", "<Leader>j", "<Plug>(easymotion-j)", { noremap = false })
+          vim.api.nvim_set_keymap("n", "<Leader>k", "<Plug>(easymotion-k)", { noremap = false })
           local wk = require("which-key")
           wk.register({
-            ["<Leader>j"] = { "<Plug>(easymotion-j)", "Find line below" },
-            ["<Leader>k"] = { "<Plug>(easymotion-k)", "Find line above" },
+               ["<Leader>j"] = { "<Plug>(easymotion-j)", "Find line below" },
+               ["<Leader>k"] = { "<Plug>(easymotion-k)", "Find line above" },
           })
           wk.register({
                ["s"] = { "<Plug>(easymotion-overwin-f2)", "Find 2 characters" },
           })
-	end,
+     end,
 }
--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,6 +1,10 @@
 return {
-  "tpope/vim-fugitive",
-  config = function()
-    vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-  end,
+     "tpope/vim-fugitive",
+     config = function()
+          vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+          local wk = require("which-key")
+          wk.register({
+               ["<leader>gs"] = { vim.cmd.Git, "Git status" },
+          })
+     end,
 }
