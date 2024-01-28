9a06625c8574f70930fa1327b82fba08a13f0daa
Andy-Zi <andreas@zieglerit.org>
2024-01-28 20:37:32 +0100

    dadbod & noice

---
+++ lua/plugins/dadbod.lua
@@ -0,0 +1,32 @@
+return{
+	{
+		"tpope/vim-dadbod",
+		config = function()
+			vim.keymap.set("n", "<leader>db", ":DBUIToggle<CR>")
+			local wk = require("which-key")
+			wk.register({
+				["<leader>db"] = { ":DBUIToggle<CR>", "DBUI" },
+			})
+		end,
+	},
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			{ "tpope/vim-dadbod", lazy = true },
+			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+		init = function()
+			-- Your DBUI configuration
+			vim.g.db_ui_use_nerd_fonts = 1
+		end,
+	},
+	{
+		"kristijanhusak/vim-dadbod-completion",
+	},
+}
