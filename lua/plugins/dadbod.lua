338a69df257325bae91f1192094c0dc246e3196c
Andy-Zi <andreas@zieglerit.org>
2024-01-29 20:47:53 +0100

    changed dadbod save location

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -37,5 +37,6 @@ return {
 		wk.register({
 			["<leader>db"] = { ":DBUIToggle<CR>", "DBUI" },
 		})
+        vim.g.db_ui_save_location = vim.fn.stdpath "config" .. require("plenary.path").path.sep .. "db_ui"
 	end,
 }
