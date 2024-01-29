a76c808e05043cfb51216db670a3fb651907c7bf
Andy-Zi <andreas@zieglerit.org>
2024-01-29 21:09:24 +0100

    auto install nonels with mason

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,22 +1,29 @@
 return {
-     "nvimtools/none-ls.nvim",
-     config = function()
-          local null_ls = require("null-ls")
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "williamboman/mason.nvim",
+        "nvimtools/none-ls.nvim",
+    },
+    config = function()
+        require("mason").setup()
+        require("mason-null-ls").setup({
+            ensure_installed = {
+                -- Opt to list sources here, when available in mason.
+            },
+            automatic_installation = false,
+            handlers = {},
+        })
+        require("null-ls").setup({
+            sources = {
+                -- Anything not supported by mason.
+            }
+        })
 
-          null_ls.setup({
-               sources = {
-                    null_ls.builtins.formatting.stylua,
-                    null_ls.builtins.formatting.prettierd,
-                    null_ls.builtins.diagnostics.eslint_d,
-                    null_ls.builtins.formatting.black,
-                    null_ls.builtins.formatting.isort,
-               },
-          })
-
-          vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-          local wk = require("which-key")
-          wk.register({
-               ["<leader>gf"] = { vim.lsp.buf.format, "Format" },
-          })
-     end,
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+        local wk = require("which-key")
+        wk.register({
+            ["<leader>gf"] = { vim.lsp.buf.format, "Format" },
+        })
+    end,
 }
