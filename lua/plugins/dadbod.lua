b18af0e428d9118f34669f1d3c7555d075056fed
Andy-Zi <andreas@zieglerit.org>
2024-01-29 20:40:05 +0100

    dadbod completions

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -19,38 +19,23 @@ return {
 				vim.g.db_ui_use_nerd_fonts = 1
 			end,
 		},
-		{ "kristijanhusak/vim-dadbod-completion" },
+		{
+			"kristijanhusak/vim-dadbod-completion",
+			config = function()
+				vim.api.nvim_create_autocmd("FileType", {
+					pattern = { "sql", "mysql", "plsql" },
+					callback = function()
+						require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
+					end,
+				})
+			end,
+		},
 	},
 	config = function()
 		vim.keymap.set("n", "<leader>db", ":DBUIToggle<CR>")
 		local wk = require("which-key")
 		wk.register({
 			["<leader>db"] = { ":DBUIToggle<CR>", "DBUI" },
-		})
-
-		vim.g.db_ui_save_location = vim.fn.stdpath("config") .. require("plenary.path").path.sep .. "db_ui"
-
-		local function db_completion()
-			require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
-		end
-
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = {
-				"sql",
-			},
-			command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
-		})
-
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = {
-				"sql",
-				"mysql",
-				"plsql"
-
-			},
-			callback = function()
-				vim.schedule(db_completion)
-			end,
 		})
 	end,
 }
