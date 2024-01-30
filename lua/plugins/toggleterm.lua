cea6fb90c4ab0f63ed63f198e180e96b9b1ad605
Andy-Zi <andreas@zieglerit.org>
2024-01-30 19:47:58 +0100

    fixed toggleterm keybinding

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,26 +1,34 @@
-return{
-    "akinsho/toggleterm.nvim",
-    config = function()
-        require("toggleterm").setup{
-            size = 20,
-            open_mapping = [[<c-\>]],
-            shade_filetypes = {},
-            shade_terminals = true,
-            shading_factor = 1,
-            start_in_insert = true,
-            persist_size = true,
-            direction = 'float',
-            close_on_exit = true,
-            shell = vim.o.shell,
-            float_opts = {
-                border = 'curved',
-                winblend = 3,
-                highlights = {
-                    border = "Normal",
-                    background = "Normal",
-                }
-            }
-        }
-        vim.api.nvim_set_keymap('n', '<leader>gl', ':lua require("toggleterm").exec("lazygit")<CR>', {noremap = true, silent = true})
-    end
+return {
+	"akinsho/toggleterm.nvim",
+	config = function()
+		require("toggleterm").setup({
+			size = 20,
+			open_mapping = [[<c-\>]],
+			shade_filetypes = {},
+			shade_terminals = true,
+			shading_factor = 1,
+			start_in_insert = true,
+			persist_size = true,
+			direction = "float",
+			close_on_exit = true,
+			shell = vim.o.shell,
+			float_opts = {
+				border = "curved",
+				winblend = 3,
+				highlights = {
+					border = "Normal",
+					background = "Normal",
+				},
+			},
+		})
+		local Terminal = require("toggleterm.terminal").Terminal
+		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+		function _lazygit_toggle()
+			lazygit:toggle()
+		end
+
+		vim.api.nvim_set_keymap("n", "<leader>gl", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+        vim.api.nvim_set_keymap("t", "<leader>gl", "<C-\\><C-n><cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+	end,
 }
