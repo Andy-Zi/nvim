7e25072a98bd8d06a95c444ed99334cacd289bd3
Andy-Zi <andreas@zieglerit.org>
2024-02-08 22:31:09 +0100

    treesiter plugins

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
+    "nvim-treesitter/nvim-treesitter",
 	dependencies = {
 		{
 			"nvim-treesitter/nvim-treesitter-context",
@@ -20,6 +20,55 @@ return {
 				})
 			end,
 		},
+		{
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			config = function()
+				require("nvim-treesitter.configs").setup({
+					textobjects = {
+						select = {
+							enable = true,
+
+							-- Automatically jump forward to textobj, similar to targets.vim
+							lookahead = true,
+
+							keymaps = {
+								-- You can use the capture groups defined in textobjects.scm
+								["af"] = "@function.outer",
+								["if"] = "@function.inner",
+								["ac"] = "@class.outer",
+								-- You can optionally set descriptions to the mappings (used in the desc parameter of
+								-- nvim_buf_set_keymap) which plugins like which-key display
+								["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+								-- You can also use captures from other query groups like `locals.scm`
+								["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+							},
+							-- You can choose the select mode (default is charwise 'v')
+							--
+							-- Can also be a function which gets passed a table with the keys
+							-- * query_string: eg '@function.inner'
+							-- * method: eg 'v' or 'o'
+							-- and should return the mode ('v', 'V', or '<c-v>') or a table
+							-- mapping query_strings to modes.
+							selection_modes = {
+								["@parameter.outer"] = "v", -- charwise
+								["@function.outer"] = "V", -- linewise
+								["@class.outer"] = "<c-v>", -- blockwise
+							},
+							-- If you set this to `true` (default is `false`) then any textobject is
+							-- extended to include preceding or succeeding whitespace. Succeeding
+							-- whitespace has priority in order to act similarly to eg the built-in
+							-- `ap`.
+							--
+							-- Can also be a function which gets passed a table with the keys
+							-- * query_string: eg '@function.inner'
+							-- * selection_mode: eg 'v'
+							-- and should return true of false
+							include_surrounding_whitespace = true,
+						},
+					},
+				})
+			end,
+		},
 	},
 	build = ":TSUpdate",
 	config = function()
@@ -30,5 +79,35 @@ return {
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
+		local vim = vim
+		local opt = vim.opt
+
+		opt.foldmethod = "expr"
+		opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+		local api = vim.api
+		local M = {}
+		-- function to create a list of commands and convert them to autocommands
+		-------- This function is taken from https://github.com/norcalli/nvim_utils
+		function M.nvim_create_augroups(definitions)
+			for group_name, definition in pairs(definitions) do
+				api.nvim_command("augroup " .. group_name)
+				api.nvim_command("autocmd!")
+				for _, def in ipairs(definition) do
+					local command = table.concat(vim.tbl_flatten({ "autocmd", def }), " ")
+					api.nvim_command(command)
+				end
+				api.nvim_command("augroup END")
+			end
+		end
+
+		local autoCommands = {
+			-- other autocommands
+			open_folds = {
+				{ "BufReadPost,FileReadPost", "*", "normal zR" },
+			},
+		}
+
+		M.nvim_create_augroups(autoCommands)
 	end,
 }
