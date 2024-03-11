9cbcd95b561cc385d8e98460bfd8b4b4d1710386
Andreas Ziegler <az@cognify.ai>
2024-03-11 11:48:36 +0100

    setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -101,7 +101,18 @@ return { -- LSP Configuration & Plugins
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                diagnosticMode = 'workspace',
+              },
+            pythonPath = './.venv/bin/python',
+            },
+          },
+      },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
