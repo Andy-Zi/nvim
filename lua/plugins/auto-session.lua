2a2d18676bccad8d491706d7f98ecf7b56836fb4
Andy-Zi <andreas@zieglerit.org>
2024-09-17 14:35:18 +0200

    don't load session in notes dir

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -14,7 +14,7 @@ return {
 			auto_session_use_git_branch = true,
 			close_unsupported_windows = true,
 			bypass_save_filetypes = {"dap_ui_scopes", "dap_repl", "dapui_watches", "dapui_breakpoints", "dapui_stacks", "dapui_console"},
-			suppressed_dirs = {"~/Code/db"},
+			suppressed_dirs = {"~/Code/db", "~/Notes"},
 		})
 		vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 		vim.keymap.set(
