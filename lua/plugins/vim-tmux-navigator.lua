fea62eb9c09e8399bd0cdc52f9803f3e304f1b18
Andy-Zi <andreas@zieglerit.org>
2024-10-25 10:25:00 +0200

    tmux navigator finally no more mapping override

--- lua/plugins/vim-tmux-navigator.lua
+++ lua/plugins/vim-tmux-navigator.lua
@@ -1,16 +1,9 @@
 return {
-     "christoomey/vim-tmux-navigator",
-     cmd = {
-          "TmuxNavigateLeft",
-          "TmuxNavigateDown",
-          "TmuxNavigateUp",
-          "TmuxNavigateRight",
-          "TmuxNavigatePrevious",
-     },
-     keys = {
-          { "<c-h>",  "<cmd>TmuxNavigateLeft<cr>" },
-          { "<c-j>",  "<cmd>TmuxNavigateDown<cr>" },
-          { "<c-k>",  "<cmd>TmuxNavigateUp<cr>" },
-          { "<c-l>",  "<cmd>TmuxNavigateRight<cr>" },
-     },
+    "christoomey/vim-tmux-navigator",
+    config = function()
+        vim.api.nvim_set_keymap("n", "<c-h>", "<cmd>TmuxNavigateLeft<cr>", { noremap = true, silent = true })
+        vim.api.nvim_set_keymap("n", "<c-j>", "<cmd>TmuxNavigateDown<cr>", { noremap = true, silent = true })
+        vim.api.nvim_set_keymap("n", "<c-k>", "<cmd>TmuxNavigateUp<cr>", { noremap = true, silent = true })
+        vim.api.nvim_set_keymap("n", "<c-l>", "<cmd>TmuxNavigateRight<cr>", { noremap = true, silent = true })
+    end,
 }
